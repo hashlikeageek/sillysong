78f8020903a1c668e5fb83f3c296c1ea2a52a407
Ashish Kumar Sai <ashishkumarsai@me.com>
2016-12-07 16:50:49 +0530

    This is the final version of the application, you can use this to generate funny lyrics.

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -2,16 +2,26 @@
 //  ViewController.swift
 //  Silly Song
 //
-//  Created by Ashutosh Kumar Sai on 07/12/16.
+//  Created by Ashish Kumar Sai on 07/12/16.
 //  Copyright © 2016 Ashish Rajendra Kumar Sai. All rights reserved.
 //
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var namefield: UITextField!
+    
+    @IBOutlet weak var lyricsview: UITextView!
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+        namefield.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +30,62 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reset(_ sender: Any) {
+        namefield.text=""
+        lyricsview.text=""
+     
+    }
+    @IBAction func displayLyrics(_ sender: Any) {
+        let finalresult = lyricsFromName(bananaFanaTemplate,fullname: namefield.text!)
+        lyricsview.text = finalresult
+    }
+  
+    
 
 }
+  
 
+
+
+func shortNameFromName (_ fullname: String)-> String{
+    let lowercaseName = fullname.lowercased()
+    let vowelSet = CharacterSet(charactersIn: "aieon")
+    
+    if let VowelRange = lowercaseName.folding(options: .diacriticInsensitive, locale: .current).rangeOfCharacter(from: vowelSet)
+    {
+        return lowercaseName.substring(from: VowelRange.lowerBound)
+    }
+        
+    else
+    {
+        return lowercaseName
+        
+    }
+}
+
+func lyricsFromName (_ lyricstemplate: String, fullname : String) -> String
+{
+    let shortname = shortNameFromName(fullname)
+    let lyrics = lyricstemplate
+        .replacingOccurrences(of: "<FULL_NAME>", with: fullname)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortname )
+    
+    return lyrics
+    
+}
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
+
+
+
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+}
+}
